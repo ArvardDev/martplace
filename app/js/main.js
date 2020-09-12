823cbf27fc6df2c79e84469aea3522d1d5c93ec8
ArvardDev <mirovygin.matrix@gmail.com>
2020-09-12 23:52:12 +0300

    added section Counts, some fixes

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,11 @@ $(function () {
 	$('#product__filter').styler();
 
 
-	$('.products__filter-btn').on('click', function(){
-			$('.products__filter-list').slideToggle();
+	$('.products__filter-btn').on('click', function () {
+		$('.products__filter-list').slideToggle();
 	});
 
-	$('.product__item-stars').rateYo({
+	$('.rate-stars').rateYo({
 		rating: 3.5,
 		maxValue: 5,
 		numStars: 5,
@@ -20,20 +20,7 @@ $(function () {
 		starSvg: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>'
 	});
 
-	$('.featured__item-stars').rateYo({
-		rating: 2.8,
-		maxValue: 5,
-		numStars: 5,
-		starWidth: "15px",
-		readOnly: true,
-		normalFill: 'transparent',
-		ratedFill: '#ffc000',
-
-		starSvg: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>'
-	})
-
 	var mixer = mixitup('.products__items');
-
 
 	$('.featured__slider').slick({
 		prevArrow: '<button type="button" class="slick-prev slick-btn"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><path fill="#000000" d="M14 20c0.128 0 0.256-0.049 0.354-0.146 0.195-0.195 0.195-0.512 0-0.707l-8.646-8.646 8.646-8.646c0.195-0.195 0.195-0.512 0-0.707s-0.512-0.195-0.707 0l-9 9c-0.195 0.195-0.195 0.512 0 0.707l9 9c0.098 0.098 0.226 0.146 0.354 0.146z"></path></svg></button>',
@@ -43,7 +30,6 @@ $(function () {
 	});
 
 	$('.followers__slider').slick({
-		// slidesToShow: 3,
 		variableWidth: true,
 		prevArrow: '<button type="button" class="slick-prev slick-btn"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><path fill="#000000" d="M14 20c0.128 0 0.256-0.049 0.354-0.146 0.195-0.195 0.195-0.512 0-0.707l-8.646-8.646 8.646-8.646c0.195-0.195 0.195-0.512 0-0.707s-0.512-0.195-0.707 0l-9 9c-0.195 0.195-0.195 0.512 0 0.707l9 9c0.098 0.098 0.226 0.146 0.354 0.146z"></path></svg></button>',
 		nextArrow: '<button type="button" class="slick-next slick-btn"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20">\
